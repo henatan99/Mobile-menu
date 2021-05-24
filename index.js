0217032892f59e83dae3f36d5704b91cbaa3e1e0
Henok <henatan99@gmail.com>
2021-05-24 12:31:08 +0300

    Add the cards section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const header = document.createElement('header');
 
 const header_h1 = document.createElement('h1');
 header_h1.setAttribute('id', 'logo');
+header_h1.innerText= 'Techno Mobile';
 
 const button = document.createElement('button');
 button.setAttribute('type', 'button');
@@ -9,8 +10,8 @@ button.innerHTML = '<i class="fas fa-bars"></i>';
 
 const header_nav = document.createElement('nav');
 header_nav.setAttribute('id', 'desktop-nav');
-const list_elems = '<li><a class="nav-link" href="#projects">Portoflio</a></li>' + 
-'<li><a class="nav-link" href="#about-me">About</a></li>' + 
+const list_elems = '<li><a class="nav-link" href="#projects">Featured</a></li>' + 
+'<li><a class="nav-link" href="#about-me">About Us</a></li>' + 
 '<li><a class="nav-link" href="#contact">Contact <i class="far fa-envelope"></i></a></li>';
 
 header_nav.innerHTML = '<ul>' + `${list_elems}` + '</ul>';
@@ -24,18 +25,15 @@ featured_div.innerHTML = '<img class="mobile-icon" src="./media/svg/icon.svg" al
 
 const featured_h2a = document.createElement('h2');
 featured_h2a.classList.add('intro-greet');
-featured_h2a.innerText = 'Hey There. <br /> I\'m Aziz';
+featured_h2a.innerText = 'Hi End Tech. <br /> Techno274';
 
 const featured_h2b = document.createElement('h2');
 featured_h2b.classList.add('intro-greet');
-featured_h2b.innerText = 'I’am a Software Developer';
+featured_h2b.innerText = 'Fine tuch features.';
 
 const featured_p = document.createElement('p');
 featured_p.classList.add('intro-text');
-featured_p.innerText = 'I can help you build a product, feature or website <br />' +
-'Look through some of my work and experience! <br />' +
-'If you like what you see and have a project you need coded, don’t' + 
-'hestiate to contact me.';
+featured_p.innerText = 'It is a high end tech mobile. Andriod OS. One of best selling items of our products';
 
 const images_src = ['./media/svg/github.svg', './media/svg/linkedin.svg', 
 './media/svg/angel.svg', './media/svg/twitter.svg', './media/svg/medium.svg'];
@@ -61,4 +59,59 @@ featured.setAttribute('id', 'intro');
 [featured_div, featured_h2a, featured_h2a, 
     featured_p, featured_social].forEach(item => featured.appendChild(item));
 
-[header, featured].forEach(item => document.body.appendChild(item));
+
+function card(classes=[], details) {
+    const div1 = document.createElement('div');
+    if(classes.length) {
+        classes.forEach(item => div1.classList.add(item));
+    }
+
+    const div2 = document.createElement('div');
+    div2.classList.add('background-div');
+
+    const div3 = document.createElement('div');
+    div3.classList.add('card-description');
+
+    div3.innerHTML = `${details}`;
+
+    [div2, div3].forEach(item => div1.appendChild(item));
+    return div1;
+}
+
+function details(p_class) {
+const innerElem = '<h2>Weather App</h2>' + 
+`<p class="${p_class}"` + 
+  'This is a responsive weather app made with JavaScript that' +
+  'displays the temperature, humidity and timezone for cities' +
+  'around the world.' +
+'</p>' + 
+'<ul class="project-stacks card-stacks">' + 
+  '<li>HTML</li>' +
+  '<li>SASS/CSS</li>' +
+  '<li>JavaScript</li>' +
+  '<li>WebPack</li>' +
+  '<li>Babel</li>' +
+'</ul>'};
+
+const card_elem1 = card(['card', 'weather'], details('white-description'));
+const card_elem2 = card(['card', 'weather'], details('white-description'));
+
+const card_lists = document.createElement('div');
+card_lists.classList.add('cards-list');
+
+card_elem1.appendChild(card_lists);
+card_elem2.appendChild(card_lists);
+
+const project = document.createElement('div');
+project.setAttribute('id', 'projects');
+project.appendChild(card_elem1);
+project.appendChild(card_elem2);
+
+
+const about_us_section = document.createElement('section');
+about_us_section.innerHTML = '<h2>About Us</h2>' + 
+'<p class="about-me-text"> We deliver high end products with reasonable price</p>';
+
+const main = document.createElement('main');
+[featured, project, about_us_section].forEach(item => main.appendChild(item));
+[header, main].forEach(item => document.body.appendChild(item));
