a4614ae4a6d88730a7e474781fe06e56d295e8d8
Henok <henatan99@gmail.com>
2021-05-25 12:55:00 +0300

    Add first cards section

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const header = document.createElement('header');
 
 const header_h1 = document.createElement('h1');
 header_h1.setAttribute('id', 'logo');
-header_h1.innerText= 'Techno Mobile';
+header_h1.innerText = 'Techno Mobile';
 
 const button = document.createElement('button');
 button.setAttribute('type', 'button');
@@ -10,18 +10,18 @@ button.innerHTML = '<i class="fas fa-bars"></i>';
 
 const header_nav = document.createElement('nav');
 header_nav.setAttribute('id', 'desktop-nav');
-const list_elems = '<li><a class="nav-link" href="#projects">Featured</a></li>' + 
-'<li><a class="nav-link" href="#about-me">About Us</a></li>' + 
-'<li><a class="nav-link" href="#contact">Contact <i class="far fa-envelope"></i></a></li>';
+const list_elems = '<li><a class="nav-link" href="#projects">Featured</a></li>'
++ '<li><a class="nav-link" href="#about-me">About Us</a></li>'
++ '<li><a class="nav-link" href="#contact">Contact <i class="far fa-envelope"></i></a></li>';
 
 header_nav.innerHTML = '<ul>' + `${list_elems}` + '</ul>';
 
-[header_h1, button, header_nav].forEach(item => header.appendChild(item));
+[header_h1, button, header_nav].forEach((item) => header.appendChild(item));
 
 const featured_div = document.createElement('div');
 featured_div.classList.add('intro-icon');
-featured_div.innerHTML = '<img class="mobile-icon" src="./media/svg/icon.svg" alt="Icon for the side"/>' +
-'<img class="desktop-icon" src="./media/svg/desktop-icon.svg" alt="Icon for the side"/>'
+featured_div.innerHTML = '<img class="mobile-icon" src="./media/svg/icon.svg" alt="Icon for the side"/>'
++ '<img class="desktop-icon" src="./media/svg/desktop-icon.svg" alt="Icon for the side"/>';
 
 const featured_h2a = document.createElement('h2');
 featured_h2a.classList.add('intro-greet');
@@ -35,17 +35,17 @@ const featured_p = document.createElement('p');
 featured_p.classList.add('intro-text');
 featured_p.innerText = 'It is a high end tech mobile. Andriod OS. One of best selling items of our products';
 
-const images_src = ['./media/svg/github.svg', './media/svg/linkedin.svg', 
-'./media/svg/angel.svg', './media/svg/twitter.svg', './media/svg/medium.svg'];
+const images_src = ['./media/svg/github.svg', './media/svg/linkedin.svg',
+  './media/svg/angel.svg', './media/svg/twitter.svg', './media/svg/medium.svg'];
 
 const a_href = ['https://github.com/SpaYco', 'https://www.linkedin.com/in/spayco',
-'https://angel.co/spayco', 'https://twitter.com/iSpaYco', 'https://spayco.medium.com/'];
+  'https://angel.co/spayco', 'https://twitter.com/iSpaYco', 'https://spayco.medium.com/'];
 
-var featured_a = '';
-for(var i=0; i<a_href.length; i+=1) {
-    featured_a +=  `<a class="social-link" href=${a_href[i]}` + 
-    `target="_blank" rel="noreferrer noopener" >` + 
-    `<img class="social-icon" src=${images_src[i]} alt="Icon"/></a>`;
+let featured_a = '';
+for (let i = 0; i < a_href.length; i += 1) {
+  featured_a += `<a class="social-link" href=${a_href[i]}`
+    + 'target="_blank" rel="noreferrer noopener" >'
+    + `<img class="social-icon" src=${images_src[i]} alt="Icon"/></a>`;
 }
 
 const featured_social = document.createElement('div');
@@ -56,62 +56,80 @@ featured_social.innerHTML = featured_a;
 const featured = document.createElement('div');
 featured.setAttribute('id', 'intro');
 
-[featured_div, featured_h2a, featured_h2a, 
-    featured_p, featured_social].forEach(item => featured.appendChild(item));
+[featured_div, featured_h2a, featured_h2a,
+  featured_p, featured_social].forEach((item) => featured.appendChild(item));
 
+function card(classes = [], details) {
+  const div1 = document.createElement('div');
+  if (classes.length) {
+    classes.forEach((item) => div1.classList.add(item));
+  }
 
-function card(classes=[], details) {
-    const div1 = document.createElement('div');
-    if(classes.length) {
-        classes.forEach(item => div1.classList.add(item));
-    }
+  const div2 = document.createElement('div');
+  div2.classList.add('background-div');
 
-    const div2 = document.createElement('div');
-    div2.classList.add('background-div');
+  const div3 = document.createElement('div');
+  div3.classList.add('card-description');
 
-    const div3 = document.createElement('div');
-    div3.classList.add('card-description');
+  div3.innerHTML = `${details}`;
 
-    div3.innerHTML = `${details}`;
-
-    [div2, div3].forEach(item => div1.appendChild(item));
-    return div1;
+  [div2, div3].forEach((item) => div1.appendChild(item));
+  return div1;
 }
 
 function details(p_class) {
-const innerElem = '<h2>Weather App</h2>' + 
-`<p class="${p_class}"` + 
-  'This is a responsive weather app made with JavaScript that' +
-  'displays the temperature, humidity and timezone for cities' +
-  'around the world.' +
-'</p>' + 
-'<ul class="project-stacks card-stacks">' + 
-  '<li>HTML</li>' +
-  '<li>SASS/CSS</li>' +
-  '<li>JavaScript</li>' +
-  '<li>WebPack</li>' +
-  '<li>Babel</li>' +
-'</ul>'};
+  const innerElem = '<h2>Weather App</h2>'
++ `<p class="${p_class}"`
+  + 'This is a responsive weather app made with JavaScript that'
+  + 'displays the temperature, humidity and timezone for cities'
+  + 'around the world.'
++ '</p>'
++ '<ul class="project-stacks card-stacks">'
+  + '<li>HTML</li>'
+  + '<li>SASS/CSS</li>'
+  + '<li>JavaScript</li>'
+  + '<li>WebPack</li>'
+  + '<li>Babel</li>'
++ '</ul>';
+};
 
 const card_elem1 = card(['card', 'weather'], details('white-description'));
-const card_elem2 = card(['card', 'weather'], details('white-description'));
 
 const card_lists = document.createElement('div');
 card_lists.classList.add('cards-list');
 
-card_elem1.appendChild(card_lists);
-card_elem2.appendChild(card_lists);
+[card_elem1].forEach(item => item.appendChild(card_lists));
+
+const about_us_section = document.createElement('section');
+about_us_section.setAttribute('id', 'about-me-section');
+
+
+const about_us = document.createElement('div');
+about_us.setAttribute('id', 'about-me');
+about_us.appendChild(about_us_section);
+
+const cards_h2 = document.createElement('h2');
+cards_h2.innerText = 'My Recent Work';
+const first_card = document.createElement('div');
+first_card.classList.add('first-card');
+first_card.innerHTML = 
+'<img src="./media/png/ubrunning.png" alt="UB-Running Screenshot" class="first-card-img"/>' +  
+'<div class="first-card-description">' +
+    '<h2>UB Running</h2><p class="description blue-color">' + 
+      'A Platform game made with JavaScript using the Phaser framework</p>'
+      '<ul class="project-stacks blue-color"> <li>HTML</li> <li>SASS/CSS</li> <li>JavaScript</li>' + 
+      '<li>WebPack</li> <li>Babel</li> <li>Phaser</li> </ul>' +
+    '<a href="https://github.com/Spayco/UB-Running/" class="link" target="_blank" '+
+    'rel="noreferrer noopener" >See project</a> </div>';
 
 const project = document.createElement('div');
 project.setAttribute('id', 'projects');
-project.appendChild(card_elem1);
-project.appendChild(card_elem2);
-
-
-const about_us_section = document.createElement('section');
-about_us_section.innerHTML = '<h2>About Us</h2>' + 
-'<p class="about-me-text"> We deliver high end products with reasonable price</p>';
+[cards_h2, first_card, card_elem1, card_elem2, card_elem3, card_elem4].forEach(item => project.appendChild(item));
 
 const main = document.createElement('main');
-[featured, project, about_us_section].forEach(item => main.appendChild(item));
-[header, main].forEach(item => document.body.appendChild(item));
+[featured, project, about_us].forEach((item) => main.appendChild(item));
+
+[header, main].forEach((item) => document.body.appendChild(item));
+
+
+
